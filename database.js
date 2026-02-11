8574798c044e3d1fe87773fb72a07adb431d466e
venividiyeeti <vitabrevix@gmail.com>
2026-02-12 00:44:11 +0100

    More files

--- database.js
+++ database.js
@@ -32,6 +32,7 @@ const audioCollections = [
             { title: "PuppyTrainerC - Puppy Brain", url: "https://media.soundgasm.net/sounds/88cd2876e056e82a3d2535647170b1bade4e6685.m4a" },
             { title: "JustMocha - My Good Kitten", url: "https://media.soundgasm.net/sounds/e3616459998c3c22f601bd75121ef0a91ce2e48b.m4a" },
             { title: "JustMocha - Simple Submission", url: "https://media.soundgasm.net/sounds/69e7b9b7262ac06b0b9d9835b69a8e1eacded2ba.m4a" },
+            { title: "Miss Lilith - Collared Obedience", url: "https://lilithunleashed.net/wp-content/uploads/2021/08/Collared-Obedience-SFX-BB..mp3" },
 		]
     ],
     [
@@ -39,13 +40,19 @@ const audioCollections = [
         "It's all about the balance~",
         [
             { title: "m0mmyl0vesy0u - Degradation Arousal Trainer", url: "https://media.soundgasm.net/sounds/ec60982676d858facf6ebdf92ba78fb129c99dbf.m4a" },
+            { title: "WilliamMastersNarrates - Praise Kink Pussy Worship", url: "https://media.soundgasm.net/sounds/d01fe07a8d08c7f822c8fc7096f882d3af0ac65d.m4a" },
         ]
     ],
     [
         "Just empty, just blank",
         "Mindless, thoughtless, unthinking, witless...~",
         [
+            { title: "nocturnowlette - Straightforward (but Intense) Trance Training", url: "https://media.soundgasm.net/sounds/7bf06e8c1a421ee359a0cab5a7a04cca5c08241e.m4a" },
             { title: "Miss Lilith - Brain Go Bye Bye", url: "https://lilithunleashed.net/wp-content/uploads/2022/04/Bye-Bye-Brain-Long-Induction-Version-MP3.mp3" },
+			{ title: "nocturnowlette - Melting Your Trance Inhibitions", url: "https://media.soundgasm.net/sounds/a7f7a965c32227bd246dd48a9834826531e1d9f6.m4a" },
+			{ title: "GoddessSoft - Slow and Gentle Hypnotic Induction", url: "https://media.soundgasm.net/sounds/0bfc1b3caa2a5a45e93dca9428ff8d981e9e69de.m4a" },
+			{ title: "Miss Lilith - Deep Conditioning – Induction", url: "https://lilithunleashed.net/wp-content/uploads/2021/08/Deep-Conditioning-Induction-Adapted-from-Commando.mp3" },
+			{ title: "Miss Lilith - Overload – Induction", url: "https://lilithunleashed.net/wp-content/uploads/2021/08/Venom-1-Induction.mp3" },
         ]
     ],
 	[
@@ -56,16 +63,50 @@ const audioCollections = [
             { title: "JustMocha - Pleasure for Playthings HFO", url: "https://media.soundgasm.net/sounds/e5e267099b50366837b02c30380f661ce651fffd.m4a" },
             { title: "JustMocha - Just Pleasure For the Villainess", url: "https://media.soundgasm.net/sounds/b8fae2de63163771bf2a465179f279425ebf64af.m4a" },
             { title: "JustMocha - Your Orgasmic Resolution", url: "https://media.soundgasm.net/sounds/fe2c5b53679be0fdb8ec0467237d5ace034c8c86.m4a" },
+            { title: "WilliamMastersNarrates - Empty Headed, Full of Pleasure", url: "https://media.soundgasm.net/sounds/228dc8be5238d2b75d04eee54c647b3282be88f1.m4a" },
+            { title: "Miss Lilith - Inhibitions Go Bye Bye", url: "https://lilithunleashed.net/wp-content/uploads/2023/01/Inhibitions-Go-Bye-Bye-Full-Long-Induction-Subby-Music-MP3.mp3" },
+            { title: "Shibbydex - Give In", url: "https://shibbydex.com/play/90876b6f-f103-428e-99e5-d7358e8fa2d9" },
+            { title: "Shibbydex - The Big Bang", url: "https://shibbydex.com/play/95373d45-d14c-429c-8055-d5efd87d0cfe" },
+            { title: "misterkace - Drop into Climax", url: "https://media.soundgasm.net/sounds/8344b68448bf49fc44f648961579566ee082f9f0.m4a" },
+            { title: "misterkace - Yes Means Yes", url: "https://media.soundgasm.net/sounds/c72a041dc23124a75f1d82a4cdc2b685b22c0413.m4a" },
+            { title: "Earesistible - Ignite Now", url: "https://media.soundgasm.net/sounds/ee29c03206cee9013ec80abeba5b313c02c5bc8f.m4a" },
+            { title: "Earesistible - Cum For Me Loop", url: "https://media.soundgasm.net/sounds/4e86246ad891343382a057c67bc9757756ad53fc.m4a" },
+            { title: "Earesistible - A Long Relaxting HFO", url: "https://media.soundgasm.net/sounds/8fbbaf55c2d71ff90352e661e239500008005293.m4a" },
+            { title: "Earesistible - Hypnotic Thrusting HFO", url: "https://media.soundgasm.net/sounds/300f4119f96f2f275a39b19455d0aaca0c5fba20.m4a" },
+            { title: "soundspell - Gentle Domme Trains You to HFO", url: "https://media.soundgasm.net/sounds/b92fcfc0fe7e400527763bf5bf64ddecb1d93172.m4a" },
+            { title: "soundspell - Always Dripping Deeper", url: "https://media.soundgasm.net/sounds/1dd6733436a15736c11bac976bdde0b2f5c79af2.m4a" },
+            { title: "kinkyshibby - HFO Hypnosis for a sweet, submissive boy", url: "https://media.soundgasm.net/sounds/ba769cdb58e75f61bc97ed518066578804960d24.m4a" },
+        ]
+    ],
+	[
+        "Not yet 😘",
+        "No release in sight~",
+        [
+            { title: "misterkace - Choose Denial - Part 1", url: "https://media.soundgasm.net/sounds/bf90119002c9c22c41cf1aa35437fc70ba43c0ed.m4a" },
+            { title: "misterkace - Choose Denial - Part 2", url: "https://media.soundgasm.net/sounds/35ae2a161b9eb1994deafaba1db665e14c28c681.m4a" },
+            { title: "Miss Lilith - Orgasm Go Bye Bye", url: "https://lilithunleashed.net/wp-content/uploads/2023/07/Orgasm-Go-Bye-Bye-Kinky-Induction-MP3.mp3" },
+            { title: "Miss Lilith - Chastity Chains", url: "https://lilithunleashed.net/wp-content/uploads/2022/07/21-Day-Chastity-Chains.mp3" },
+            { title: "Miss Lilith - Locked by a Bimbo", url: "https://lilithunleashed.net/wp-content/uploads/2024/06/Locked-by-a-Bimbo-MP3.mp3" },
+            { title: "Shibbydex - Caged & Fucked into Submission", url: "https://shibbydex.com/play/99953221-3d2d-4ccf-8da9-1261303c6213" },
+			
         ]
     ],
     [
         "Different, yet so familiar",
         "Just whatever, go wild~",
         [
+			{ title: "nocturnowlette - Safer is Deeper", url: "https://media.soundgasm.net/sounds/180a2b301e3b872697a415526fe988e2d87f22da.m4a" },
             { title: "GoddessSoft - You're so good at this game!", url: "https://media.soundgasm.net/sounds/a0cc34647dd816beaa03ab384e681421c1c9c391.m4a" },
             { title: "GoddessSoft - The Scent of Submission", url: "https://media.soundgasm.net/sounds/42cbd7ca71f76c6f73cd4a60560798fdfe7ca845.m4a" },
             { title: "GoddessSoft - Optimizing your Mind for Maximized Performance", url: "https://media.soundgasm.net/sounds/eb6ecb3da456d9f1a5875fd705c2ed3b7d486a56.m4a" },
             { title: "JustMocha - At the Feet of the Villainess", url: "https://media.soundgasm.net/sounds/db7325248dfdeb19f20a8448d380a16c43ce439a.m4a" },
+            { title: "WilliamMastersNarrates - Good Girls Practice Orgasm Control", url: "https://media.soundgasm.net/sounds/3143dec101ecdde28aab521bfc9278237f5207e8.m4a" },
+            { title: "WilliamMastersNarrates - Hypno for More Sensitive Nipples", url: "https://media.soundgasm.net/sounds/d492b35b9891ef6fdbd5f69d008267a5e1d9a501.m4a" },
+			{ title: "nocturnowlette - Sleep Orb", url: "https://media.soundgasm.net/sounds/9d91e74d3614f04b7b2d9d8c1b1c9ac6a1ba2f9e.m4a" },
+			{ title: "misterkace - My Sweet Girl", url: "https://media.soundgasm.net/sounds/5933d6a2adc086f7fb522cebc8cd99e5504f6606.m4a" },
+			{ title: "Shibbydex - Domina's Outlet", url: "https://shibbydex.com/play/970d65b2-e5a5-4ef2-88b5-90365ea53cc1" },
+			{ title: "Miss Lilith - Kinky Workout", url: "https://lilithunleashed.net/wp-content/uploads/2023/04/Kinky-Workout-Submission-MP3.mp3" },
+			{ title: "Gingerbreadpigge - Wellness Retreat", url: "https://media.soundgasm.net/sounds/e75241424774a5d301aad28147354fba78bf67c8.m4a" },
         ]
     ]
 ];
